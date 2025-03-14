ba3bf704a97564e338f0629de6e4df542216225b
Shahrose Kasim <rosemaster3000@gmail.com>
2025-03-14 09:36:51 -0400

    added eCPM api

---
+++ PaypalBridge/ecpm.py
@@ -0,0 +1,55 @@
+from PaypalBridge.SECRET import UNITY_MONETIZATION_KEY, UNITY_ORGINIZATION_KEY
+import requests
+import json
+import datetime
+
+# Fetch the eCPM average from yesterday - today
+# resolution : hour, day, week, month, year, all
+# dayRange   : how many days from today in past to query
+def get_ecpm(resolution="week", dayRange=1):
+    base_url = f"https://monetization.api.unity.com/stats/v1/operate/organizations/{UNITY_ORGINIZATION_KEY}"
+
+    today = datetime.date.today()
+    yesterday = today - datetime.timedelta(days=dayRange)
+
+    params = {
+        "fields": "adrequest_count,start_count,view_count,available_sum,revenue_sum",
+        "scale": resolution,
+        "groupBy": "placement",
+        "start": yesterday.isoformat(),
+        "end": today.isoformat()
+    }
+
+    headers = {
+        "Authorization": f"Token {UNITY_MONETIZATION_KEY}",
+        "Accept": "application/json",
+    }
+
+    try:
+        response = requests.get(base_url, params=params, headers=headers)
+        response.raise_for_status()
+        return response.json()
+        
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error querying Unity Ads API: {e}")
+        if response is not None:
+            print(f"Response code: {response.status_code}")
+            try:
+                print(response.json())
+            except json.JSONDecodeError:
+                print(response.text)
+        return None
+
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON response: {e}")
+        print(response.text)
+        return None
+
+
+def test():
+    data = get_ecpm(resolution='day', dayRange=30)
+    print(data)
+
+    with open('ecpm.json', 'w') as file:
+        json.dump(data, file, indent=4)
--- test.py
+++ test.py
@@ -1,15 +1,18 @@
 # from PaypalBridge.paypal import test_payout
 # test_payout()
 
+from PaypalBridge.ecpm import test
+test()
 
-from PaypalBridge.website import minimal_ad_count
-assert minimal_ad_count(r=10,i=10, gems=101)  == (10, 1)
-assert minimal_ad_count(r=1, i=10, gems=20)   == (1, 10)
-assert minimal_ad_count(r=1, i=10, gems=21)   == (None, None)
-assert minimal_ad_count(r=1, i=9, gems=20)    == (None, None)
-assert minimal_ad_count(r=2, i=0, gems=15)    == (2, 0)
-assert minimal_ad_count(r=3, i=1, gems=10000) == (None, None)
-print("All tests passed ✅")
+
+# from PaypalBridge.website import minimal_ad_count
+# assert minimal_ad_count(r=10,i=10, gems=101)  == (10, 1)
+# assert minimal_ad_count(r=1, i=10, gems=20)   == (1, 10)
+# assert minimal_ad_count(r=1, i=10, gems=21)   == (None, None)
+# assert minimal_ad_count(r=1, i=9, gems=20)    == (None, None)
+# assert minimal_ad_count(r=2, i=0, gems=15)    == (2, 0)
+# assert minimal_ad_count(r=3, i=1, gems=10000) == (None, None)
+# print("All tests passed ✅")
 
 
 # from PaypalBridge.database.tinydb import purge_redeemed_ads
