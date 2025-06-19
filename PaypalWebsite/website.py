7fce95c560a44328bd88f767ddf8ccb236053c58
Shahrose Kasim <rosemaster3000@gmail.com>
2025-06-18 23:02:53 -0400

    Update website.py

--- PaypalWebsite/website.py
+++ PaypalWebsite/website.py
@@ -21,9 +21,12 @@ import magic
 app = Flask(__name__) 
 bcrypt = Bcrypt(app)
 app.config['SECRET_KEY'] = SECRET.FLASK_KEY
-app.config['SESSION_COOKIE_SAMESITE'] = 'None'
-app.config['SESSION_COOKIE_SECURE'] = True
-
+if not app.debug:
+    print("PROD")
+    app.config['SESSION_COOKIE_SAMESITE'] = 'None'
+    app.config['SESSION_COOKIE_SECURE'] = True
+else:
+    print("DEBUG")
 
 # initialize storage folders
 app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, '..', 'uploads')
