4018d8e92bd7a378647b8e6bd5bafd886b50938f
Shahrose Kasim <rosemaster3000@gmail.com>
2025-06-19 15:22:50 -0400

    Update website.py

--- PaypalWebsite/website.py
+++ PaypalWebsite/website.py
@@ -25,7 +25,7 @@ app.config['SECRET_KEY'] = SECRET.FLASK_KEY
 
 
 # Cookie hotfixes
-CORS(api, resources={r"/api/*": {"origins": "*"}})
+CORS(app)
 # if not app.debug:
 #     app.config['SESSION_COOKIE_SAMESITE'] = 'None'
 #     app.config['SESSION_COOKIE_SECURE'] = True
